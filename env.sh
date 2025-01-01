fab4cdd378fdc6b9864a3db44be586ebc7c56905
QINGTIAN <pxh7996@gmail.com>
2025-01-01 10:35:53 +0000

    add

--- env.sh
+++ env.sh
@@ -1,12 +1,14 @@
 
-sudo apt-get -y update
-sudo apt-get -y install swig
+sudo apt-get update
+
 sudo apt-get -y install libsuitesparse-dev
+sudo apt-get -y install build-essential
+sudo apt-get -y install swig
 
 cd /workspace/wh_demo/scikit-umfpack
-pip install -e .
+pip install .
 
 cd /workspace/wh_demo/pyfrbus
-pip install pandas scipy numpy black flake8 mypy typing_extensions multiprocess sympy==1.3 symengine matplotlib lxml networkx
+pip install .
 
 
